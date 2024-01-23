5e6c81475b7227b11f9079ed4fd2b9bf009a0421
mpianella <pianellamatteo@gmail.com>
2024-01-23 11:07:24 +0100

    solved the first exercise

--- PS1_pianella.R
+++ PS1_pianella.R
@@ -16,4 +16,15 @@
 ##
 ## ---------------------------
 
-# 
+# exercise 4(a) -------
+## I run a simulation for 1000 cities using a binomial distribution
+
+s_bar <- .556
+n_cities_below_6000 <- 102
+set.seed(314)
+n_below_6000 <- rbinom(1000, n_cities_below_6000, s_bar)
+s_c <- n_below_6000/102 # calculate s_c for each city 
+sd_s_c <- sqrt((sum((s_c - s_bar)^2))/n_cities_below_6000)
+print(sd_s_c)
+
+
