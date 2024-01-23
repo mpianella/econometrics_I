c70d7f258458c491aaac89b8d53f2a1429c724c2
mpianella <pianellamatteo@gmail.com>
2024-01-23 11:26:56 +0100

    update to exercise 4(a)

--- PS1_pianella.R
+++ PS1_pianella.R
@@ -17,14 +17,17 @@
 ## ---------------------------
 
 # exercise 4(a) -------
-## I run a simulation for 1000 cities using a binomial distribution
+## I run a simulation of 819 cities using a binomial distribution
 
 s_bar <- .556
-n_cities_below_6000 <- 102
+n_cities_between5500_and_6500 <- 102
 set.seed(314)
-n_below_6000 <- rbinom(1000, n_cities_below_6000, s_bar)
+n_sim <- 819
+n_below_6000 <- rbinom(n_sim, n_cities_between5500_and_6500, s_bar)
 s_c <- n_below_6000/102 # calculate s_c for each city 
-sd_s_c <- sqrt((sum((s_c - s_bar)^2))/n_cities_below_6000)
-print(sd_s_c)
+sd_s_c <- sqrt((sum((s_c - s_bar)^2))/n_sim)
+print(sd_s_c) # my sd is consistently lower than what they have in Table A1 (0.118)
+
+
 
 
