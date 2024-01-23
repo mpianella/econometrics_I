9bac7bd89555d67ac9b8b7d27425d7d5f6bb4e18
mpianella <pianellamatteo@gmail.com>
2024-01-23 10:44:21 +0100

    add header to script

---
+++ PS1_pianella.R
@@ -0,0 +1,19 @@
+## ---------------------------
+##
+## Script name: PS1_pianella.R
+##
+## Purpose of script: to provide solutions for the progamming part of the first
+##                    problem set in econometrics I
+##                  
+## Author: Matteo Pianella
+##
+## Date Created: 23rd January 2024
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+# 
