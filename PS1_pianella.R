ba3e2b9e41f461b5d0d64f33a3dfb78d3a37a243
mpianella <pianellamatteo@gmail.com>
2024-01-25 21:01:43 +0100

    solved exercise 4.b.4

--- PS1_pianella.R
+++ PS1_pianella.R
@@ -19,6 +19,8 @@
 library(dplyr)
 library(ggplot2)
 
+
+
 # ex 4.a -------
 ## I run 819 simulations using a binomial distribution
 s_bar <- .556
@@ -33,33 +35,27 @@ print(c(sd_s_c, min(s_c), max(s_c))) # my sd is consistently lower than what the
 # ex 4.b.1 ------
 ## I use a binomial distribution to run 100 simulation for each possible scenario of N
 N <- c(50, 100, 250, 500, 1000)
-p <- 1/2 # this is the probability of selecting a female in a Bernoulli trial 
+p <- 1/2 # this is the probability of selecting a male in a Bernoulli trial 
 k <- 40  # number of students in each class
 beta <- 1/3
 n_sim <- 100
-d_beta_hat <- list()
-sigma_beta <- list()
 get_sd <- function(N, p, k, beta, n_sim){
+    sigma_beta <- list()
     for(i in 1:length(N)){
         d <- c()
         for(j in 1: n_sim){
-            n_female_p_class <- rbinom(N[i], size = k, prob = p) # generate a list of values for each possible scenario of N
-            fract_fem_p_class <- n_female_p_class/k
+            n_male_p_class <- rbinom(N[i], size = k, prob = p) # generate a vector of number of males for each class
+            fract_male_p_class <- n_male_p_class/k
             epsilon <- rnorm(N[i])  # generate a list of values for epsilon
-            y <- beta * fract_fem_p_class + epsilon
+            y <- beta * fract_male_p_class + epsilon
             data_lm <- data_frame(y, epsilon)
-            model <- lm(y ~ 0 + fract_fem_p_class)
+            model <- lm(y ~ 0 + fract_male_p_class)
             x <- coef(model)
             d <- rbind(d, x)
         }
-        # d_beta_hat[[i]] <- data.frame(beta_hat = d, size = rep(N[i], nrow(d)), row.names = NULL)
         sigma_beta[[i]] <- sd(d)
     }
-    # # calculate standard deviations
-    # sigma_beta <- do.call(rbind, lapply(d_beta_hat, function(x) sd(x[[1]])))
-    # return(sigma_beta)
     return(sigma_beta)
-    
 }
 set.seed(3141)
 sigma_beta <- get_sd(N = N ,p=p,k=k,beta=beta,n_sim=n_sim)
@@ -71,29 +67,103 @@ p4b1 <- ggplot(d_plot_4b1, aes(x = N, y = sigma_beta)) +
 p4b1
 
 # ex 4.b.2 ----
-# set up coefficients again
+# set up coefficients 
 N <- c(50, 100, 250, 500, 1000)
-p <- 1/2 # this is the probability of selecting a female in a Bernoulli trial 
-k <- 40  # number of students in each class
+p <- 1/2 # this is the probability of selecting a male in a Bernoulli trial 
+k <- c(10, 20, 60) # vector with number of students for each class
 beta <- 1/3
-n_sim <- 1500
-d_beta_hat <- list()
+n_sim <- 100
 sigma_beta <- list()
-k <- c(10, 20, 60)
 data <- data_frame()
 for(w in 1:length(k)){
-    sigma_beta <- get_sd(N = N, p=p, k = k[w], beta = beta, n_sim = n_sim2)
+    sigma_beta <- get_sd(N = N, p=p, k = k[w], beta = beta, n_sim = n_sim)
     d <- data_frame(sigma_beta = unlist(sigma_beta), size = N, class_size = k[w])
     data <- rbind(data, d)
 }
-
 d <- data_frame(d_plot_4b1, class_size = 40)
 data_plot_4b2 <- rbind(d, data)
 
-p4b2 <- ggplot(data_plot_4b2, aes(x = size, y = sigma_beta, color = class_size)) +
+p4b2 <- ggplot(data_plot_4b2, aes(x = size, y = sigma_beta, color = factor(class_size))) +
     geom_point() +
+    geom_line() +
     theme_minimal() +
+    scale_color_manual(values = c("#1f77b4", "#ff7f0e", "#2ca02c", "#9467bd")) +  
     labs(title = "Number of classes and standard deviation of beta coefficient", x = "N", y = "Sigma beta")
 p4b2
 
 
+#ex 4.b.4 -------
+# calculate standard deviation for the estimate of her initial sample
+N <- c(200)
+k <- c(30)
+p <- 1/2
+beta <- 1/3
+n_sim <- 100
+sigma_beta <- list()
+set.seed(31415)
+sigma_beta <- get_sd(N = N, p=p, k = k, beta = beta, n_sim = n_sim)
+print(sigma_beta)
+ex_ante_data <- data_frame(sigma_beta = unlist(sigma_beta), N = 200, funding = "ex_ante")
+
+
+# calculate the standard deviation when she uses funding in urban area
+# modify the function to return beta_hat instead of sigma_beta
+get_beta_hat <- function(N, p, k, beta, n_sim){
+    beta_hat <- list()
+    for(i in 1:length(N)){
+        d <- c()
+        for(j in 1: n_sim){
+            n_male_p_class <- rbinom(N[i], size = k, prob = p) # generate a vector of number of males for each class
+            fract_male_p_class <- n_male_p_class/k
+            epsilon <- rnorm(N[i])  # generate a list of values for epsilon
+            y <- beta * fract_male_p_class + epsilon
+            data_lm <- data_frame(y, epsilon)
+            model <- lm(y ~ 0 + fract_male_p_class)
+            x <- coef(model)
+            d <- rbind(d, x)
+        }
+        beta_hat <- d
+    }
+    return(beta_hat)
+}
+
+N <- c(200, 100)
+k <- c(30, 40)
+p <- 1/2
+beta <- 1/3
+n_sim <- 100
+data <- data_frame()
+for(w in 1:length(k)){
+    beta_hat <- get_beta_hat(N = N[w], p=p, k = k[w], beta = beta, n_sim = n_sim)
+    d <- data_frame(beta_hat = unlist(beta_hat), size = N[w], class_size = k[w])
+    data <- rbind(data, d)
+}
+set.seed(314159)
+sigma_beta <- sd(data$beta_hat)
+urban_data <- data_frame(sigma_beta = sigma_beta, N = 300, funding = "urban")
+
+# run the same exercise for rural
+
+N <- c(200, 50)
+k <- c(30, 15)
+p <- 1/2
+beta <- 1/3
+n_sim <- 100
+data <- data_frame()
+for(w in 1:length(k)){
+    beta_hat <- get_beta_hat(N = N[w], p=p, k = k[w], beta = beta, n_sim = n_sim)
+    d <- data_frame(beta_hat = unlist(beta_hat), size = N[w], class_size = k[w])
+    data <- rbind(data, d)
+}
+set.seed(3141593)
+sigma_beta <- sd(data$beta_hat)
+rural_data <- data_frame(sigma_beta = sigma_beta, N = 250, funding = "rural")
+
+d_plot_4b4 <- rbind(ex_ante_data, urban_data, rural_data)
+
+p4b4_1 <- ggplot(d_plot_4b4, aes(x = factor(N), y = sigma_beta, fill = factor(funding))) +
+    geom_bar(stat = "identity", position = "dodge") +
+    theme_minimal() +
+    scale_fill_manual(values = c("#1f77b4", "#ff7f0e", "#9467bd")) +  
+    labs(title = "Number of classes and standard deviation of beta coefficient", x = "N", y = "Sigma beta")
+p4b4_1
