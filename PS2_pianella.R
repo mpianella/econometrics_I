3a8651a8be4621f11855a43217e122436bca96d7
mpianella <pianellamatteo@gmail.com>
2024-01-30 16:07:40 +0100

    created new script

---
+++ PS2_pianella.R
@@ -0,0 +1,19 @@
+## ---------------------------
+##
+## Script name: PS2_pianella.R
+##
+## Purpose of script: to solve the second problem set for the PhD course econometrics I
+##
+## Author: Matteo Pianella
+##
+## Date Created: 30th Jan 24
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+# ex.3.e --------
+
