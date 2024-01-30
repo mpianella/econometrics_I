b7dbd09332027e103d4c2c585b26977023c57500
mpianella <pianellamatteo@gmail.com>
2024-01-30 16:35:56 +0100

    create a function for the first cumulative density function

--- PS2_pianella.R
+++ PS2_pianella.R
@@ -16,4 +16,25 @@
 ## ---------------------------
 
 # ex.3.e --------
+n <- c(50, 100, 250, 1000)
 
+#' Compute the cumulative density when lambda equals to the MLE estimate
+#' 
+#' @param {obs} {a vector of observations}
+#' @param {x_tilde} {a scalar that defines value the argument in the cumulative distribution function}
+comp_cfd_expon_1 <- function(obs, x_tilde) {
+    mean_obs <- mean(obs, na.rm = TRUE) 
+    lambda_mle <- 1/mean_obs # define lambda mle as the inverse of the sample mean
+    value_cdf <- 1 - exp(-lambda_mle * x_tilde)
+    return(value_cdf)
+}
+
+# sample n observations from the exponential distribution with lambda = 1
+lambda <- 1
+obs <- list()
+obs <- lapply(n, function(x) rexp(x, rate = lambda))
+
+
+
+x_tilda <- c(1/2, 1, 2, 3)
+n_iter <- 
